4da3c66797db68ac923353ff11f7f3dc186ac3ae
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-01-23 20:36:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -27,9 +27,57 @@ for(var i=0;i<drumLength;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){     
        // alert("i got tipuni");
 
-       var audio = new Audio("sounds/tom-1.mp3");
-             audio.play();
+       var buttonInnerHTML = this.innerHTML;  // i=0 hole w ke target korbe 
 
+
+       switch (buttonInnerHTML) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+            break;
+
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+                break;
+
+                case "s":
+                    var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+                    break;
+
+                    case "d":
+                        var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                        break;
+
+                        case "j":
+                            var audio = new Audio("sounds/snare.mp3");
+                    audio.play();
+                            break;
+
+                            case "k":
+                                var audio = new Audio("sounds/crash.mp3");
+                        audio.play();
+                                break;
+
+                                case "l":
+                                    var audio = new Audio("sounds/kick-bass.mp3");
+                            audio.play();
+                                    break;
+
+       
+        default: 
+        console.log(buttonInnerHTML);
+
+
+            
+       }
+
+      
 
     } );
     } 
+
+
+    
